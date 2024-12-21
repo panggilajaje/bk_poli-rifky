e8e20fcb47e95865d3d8307b7c6e8d63da806c09
panggilajaje <rifkydragneel845@gmail.com>
2024-12-21 23:05:43 +0700

    new repo BK

---
+++ config/url.php
@@ -0,0 +1,5 @@
+<?php
+$base_admin = "http://" . $_SERVER['HTTP_HOST'] . "/bk_poliklinik-main/pages/admin";
+$base_pasien = "http://" . $_SERVER['HTTP_HOST'] . "/bk_poliklinik-main/pages/pasien";
+$base_dokter = "http://" . $_SERVER['HTTP_HOST'] . "/bk_poliklinik-main/pages/dokter";
+?>
